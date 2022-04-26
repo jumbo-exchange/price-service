f2bcf0d9eac302a179ffa2bc06f3059b5e5d02c3
Solniechniy <korniichuk.oleksandr@airslate.com>
2022-04-26 13:24:50 +0300

    fix: typo

--- src/constants.ts
+++ src/constants.ts
@@ -3,4 +3,4 @@ export const DEFAULT_PAGE_LIMIT = 100;
 export const LOW_LIQUIDITY_POOL_VOLUME = 1000;
 export const EMPTY_POOL_VOLUME = 0;
 export const TO_FIXED_5_PRECISION = 5;
-export const TO_FIXED_0_PRECISION = 5;
+export const TO_FIXED_0_PRECISION = 0;
